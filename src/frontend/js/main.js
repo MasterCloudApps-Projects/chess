4838f7af1c48a0c69aecc3832ff1a9c9f49f4c3d
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-08-08 21:19:08 +0200

    feature: closure encapsulating temporal frontend variable

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -2,32 +2,39 @@ function getBackendURL(endpoint = '') {
     return 'http://localhost:3000' + endpoint;
 }
 
-let movementOriginTemp;
+function createMovementManagement() {
+    let movementOriginTemp;
 
-function selectPositionForMovement(positionId) {
-    if (movementOriginTemp === undefined) {
-        movementOriginTemp = positionId;
-        return;
-    }
-    //TODO: Maybe use a bundler like webpack so que can use axios here
-    $.ajax({
-        url: getBackendURL('/move'),
-        method: 'POST',
-        data: JSON.stringify({
-            movementOrigin: movementOriginTemp,
-            movementDestination: positionId
-        }),
-        contentType: 'application/json; charset=utf-8',
-        success: (newBoardData) => {
-            movementOriginTemp = undefined;
-            paintBoardOnHTML(newBoardData);
-        },
-        error: (err) => {
-            // Movement wasn't valid (moving empty cell, a not owned piece, other cases)
-            // TODO: Show error on HTML
-            console.log(err);
+    function selectPositionForMovement(positionId) {
+        if (movementOriginTemp === undefined) {
+            movementOriginTemp = positionId;
+            return;
         }
-    });
+        //TODO: Maybe use a bundler like webpack so que can use axios here
+        $.ajax({
+            url: getBackendURL('/move'),
+            method: 'POST',
+            data: JSON.stringify({
+                movementOrigin: movementOriginTemp,
+                movementDestination: positionId
+            }),
+            contentType: 'application/json; charset=utf-8',
+            success: (newBoardData) => {
+                movementOriginTemp = undefined;
+                paintBoardOnHTML(newBoardData);
+            },
+            error: (err) => {
+                // Movement wasn't valid (moving empty cell, a not owned piece, other cases)
+                // TODO: Show error on HTML
+                console.log(err);
+            }
+        });
+    };
+
+    return {
+        selectPositionForMovement
+    };
+    
 }
 
 function paintBoardOnHTML(boardData) {
@@ -39,8 +46,9 @@ function paintBoardOnHTML(boardData) {
 }
 
 function setCellOnClickEvents() {
+    let movementManagement = createMovementManagement();
     $('.cell').each(function(i, cell) {
-        cell.onclick = function() { selectPositionForMovement(cell.id) };
+        cell.onclick = function() { movementManagement.selectPositionForMovement(cell.id) };
     });
 }
 
