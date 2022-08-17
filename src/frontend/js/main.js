e448aec0ca139700881eb18d0f5e99ec0d04af69
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-08-17 22:03:41 +0200

    feature: UUID generation and usage on game creation & movement requests

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -2,10 +2,37 @@ function getBackendURL(endpoint = '') {
     return 'http://localhost:3000' + endpoint;
 }
 
-function createMovementManagement() {
+function initializeGameManagement() {
     let movementOriginTemp;
+    let gameUUID;
+
+    function initializeGame() {
+        gameUUID = crypto.randomUUID()
+        $.ajax({
+            url: getBackendURL('/game'),
+            method: 'POST',
+            data: JSON.stringify({
+                gameUUID: gameUUID
+            }),
+            contentType: 'application/json; charset=utf-8',
+            success: (data) => {
+                console.log('Game successfully initialized');
+            },
+            error: (err) => {
+                console.log('Error initializing game, trying again...');
+                gameUUID = undefined;
+                setTimeout(() => {initializeGame()}, 1500);
+            }
+        });
+    }
+
+    function getGameUUID() {
+        return gameUUID;
+    }
 
     function selectPositionForMovement(positionId) {
+        if (gameUUID === undefined)
+            return;
         if (movementOriginTemp === undefined) {
             movementOriginTemp = positionId;
             return;
@@ -14,6 +41,7 @@ function createMovementManagement() {
             url: getBackendURL('/move'),
             method: 'POST',
             data: JSON.stringify({
+                gameUUID: gameUUID,
                 movementOrigin: movementOriginTemp,
                 movementDestination: positionId
             }),
@@ -31,6 +59,8 @@ function createMovementManagement() {
     };
 
     return {
+        initializeGame,
+        getGameUUID,
         selectPositionForMovement
     };
 
@@ -44,11 +74,12 @@ function paintBoardOnHTML(boardData) {
          }
 }
 
-function setCellOnClickEvents() {
-    let movementManagement = createMovementManagement();
+function prepareGameAndClickEvents() {
+    let gameManager = initializeGameManagement();
+    gameManager.initializeGame();
     $('.cell').each(function(i, cell) {
-        cell.onclick = function() { movementManagement.selectPositionForMovement(cell.id) };
+        cell.onclick = function() { gameManager.selectPositionForMovement(cell.id) };
     });
 }
 
-setCellOnClickEvents();
+prepareGameAndClickEvents();
--- src/patternsRouter.js
+++ src/patternsRouter.js
@@ -3,10 +3,15 @@ import { __dirname } from './dirname.js';
 
 const router = express.Router();
 
+router.post('/game', (req, res) => {
+    console.log('Creating game with UUID ' + req.body.gameUUID);
+    res.status(200).send();
+});
+
 router.post('/move', (req, res) => {
     console.log(req.body);
     let boardResponse = {};
-    for (let i = 1; i <= 8; i++) 
+    for (let i = 1; i <= 8; i++)
         for (let letter = 0; letter < "abcdefgh".length; letter++) {
             let currentID = "abcdefgh"[letter] + i.toString();
             boardResponse[currentID] = "WR";
